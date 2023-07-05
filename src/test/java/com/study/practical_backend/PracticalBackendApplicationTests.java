236d1885bdaef3ac933ebe9b9baf6dea11e0e3df
SangBeom-Hahn <hsb990917@gmail.com>
2023-07-05 19:55:35 +0900

    Initial commit

---
+++ src/test/java/com/study/practical_backend/PracticalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.practical_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
